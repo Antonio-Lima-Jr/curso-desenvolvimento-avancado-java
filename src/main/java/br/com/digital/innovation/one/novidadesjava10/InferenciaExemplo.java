2d449bcc2615b8ed68df18b7615b003975175930
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-05-23 14:46:20 -0300

    Novidades do java 10 - inferência de tipos com a palavra reservada var e  Try with resource - Para fechar conexões automaticamente

---
+++ src/main/java/br/com/digital/innovation/one/novidadesjava10/InferenciaExemplo.java
@@ -0,0 +1,59 @@
+package br.com.digital.innovation.one.novidadesjava10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+/**
+ * Class Description.
+ *
+ * @author Antônio Lima Jr
+ * @project curso-desenvolvimento-avancado-em-java
+ * @created 23/mai/2021 - 12:36
+ */
+public class InferenciaExemplo {
+
+
+  public static void main(String[] args) throws IOException {
+
+    testar("João", "Paulo");
+    printerSoma(10, 20);
+
+  }
+
+  private static void printerSoma(int a, int b) {
+    var resultado = a + b;
+    System.out.println(resultado);
+  }
+
+  private static void connectAndPrintURLJavaOracle() throws IOException {
+    var url = new URL("https://docs.oracle.com/javase/10/language/");
+    var urlConnection = url.openConnection();
+    /*
+    Try with resource - quem implementar a classe Closeable, pode ser passada para o parentheses abaixo
+    para ser fechado automaticamente quando sair do try;
+     */
+    try (var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+      System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+    }
+
+  }
+
+  private static void testar(String nome, String sobrenome) {
+    var nomeCompleto = nome + sobrenome;
+    System.out.println(nomeCompleto);
+  }
+  // Conseguimos
+
+  // var sempre deve estar dentro de um escopo, um método, um while, for, if, else ...
+  // variável suporte do enhanced for
+  // variável suporte do for iterativo
+
+  // Não conseguimos
+
+  // var não pode ser utilizado em nível de classe
+  // var não pode ser utilizado como parâmetro de métodos
+  // Nao pode ser utilizada em variáveis locais não inicializada
+}
